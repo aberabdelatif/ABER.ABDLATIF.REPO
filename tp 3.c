fccb4fab5543e39d2613c0f522e724d04b7b0fff
Abdoaber <126400368+aberabdelatif@users.noreply.github.com>
2023-03-22 11:45:47 +0100

    Algorithme de tri
    4 fonctions/procédures déférèrent pour trier un tableau (un
    seul dimension) en ordre descendent.

---
+++ tp 3.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+void quick_sort(int arr[], int low, int high);
+int partition(int arr[], int low, int high);
+
+int main() {
+    int rows, cols;
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+    printf("Enter number of columns: ");
+    scanf("%d", &cols);
+    
+    int arr[rows][cols];
+    printf("Enter the values:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+    
+    // Sort each row separately
+    for (int i = 0; i < rows; i++) {
+        quick_sort(arr[i], 0, cols - 1);
+    }
+    
+    printf("The sorted array is:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
+
+void quick_sort(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+        quick_sort(arr, low, pi - 1);
+        quick_sort(arr, pi + 1, high);
+    }
+}
+
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+    for (int j = low; j < high; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+    int temp = arr[i+1];
+    arr[i+1] = arr[high];
+    arr[high] = temp;
+    return i + 1;
+}
